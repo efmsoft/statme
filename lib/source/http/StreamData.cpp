c0246b66c72345adff18f7b9086d2cbc190a4af2
Eduard Mishkurov <emishkurov@gmail.com>
2024-10-05 17:06:30 +0300

    remove reserver call

--- lib/source/http/StreamData.cpp
+++ lib/source/http/StreamData.cpp
@@ -1,15 +1,6 @@
-#include <cassert>
 #include <string.h>
 
 #include <Statme/http/StreamData.h>
-
-static const size_t BLOCK_SIZE = 64UL * 1024;
-
-static size_t AlignSize(size_t n)
-{
-  size_t blocks = ((n + BLOCK_SIZE - 1) / BLOCK_SIZE);
-  return blocks * BLOCK_SIZE;
-}
 
 StreamData::StreamData(size_t prealloc)
   : std::vector<char>()
@@ -39,14 +30,6 @@ void StreamData::Append(const void* data, size_t n)
     size_t cb = size();
     size_t size = cb + n;
     
-    if (size > capacity())
-    {
-      size_t ncb = AlignSize(size);
-      assert(ncb >= size);
-
-      reserve(ncb);
-    }
-
     resize(size);
     memcpy(&(*this)[cb], data, n);
   }
