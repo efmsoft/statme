cf0502d75c7274823c99c14f6ffa927bf0dd3dea
Alexander Stepanov <alexander_mail@inbox.ru>
2025-03-09 15:47:54 +0300

    Regenerate cpp/h file in AsUCharArray if original file is changed

--- lib/AsUCharArray.cmake
+++ lib/AsUCharArray.cmake
@@ -1,12 +1,17 @@
 function(AsUCharArray in_file array_var row_size out_cpp_file out_hpp_file)
-    file(SIZE ${CMAKE_CURRENT_SOURCE_DIR}/${in_file} file_size)
-    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/${in_file} file_content HEX)
-    math(EXPR hex_size "${file_size} * 2")
+    set(input_file_path "${CMAKE_CURRENT_SOURCE_DIR}/${in_file}")
+    set(stamp_file "${CMAKE_BINARY_DIR}/${array_var}_stamp.txt")
 
+    configure_file(${input_file_path} ${stamp_file} COPYONLY)
+
+    file(SIZE ${input_file_path} file_size)
+    file(READ ${input_file_path} file_content HEX)
+
+    math(EXPR hex_size "${file_size} * 2")
     set(array_content "std::array<unsigned char, ${file_size}> ${array_var} = {\n  ")
     set(line_length 0)
-
     math(EXPR row_size "${row_size} * 2")
+
     foreach(index RANGE 0 ${hex_size} 2)
         if(index EQUAL hex_size)
             string(APPEND array_content "\n};")
@@ -27,7 +32,7 @@ function(AsUCharArray in_file array_var row_size out_cpp_file out_hpp_file)
 
     file(WRITE ${header_file} "#pragma once\n\n#include <array>\n\nextern std::array<unsigned char, ${file_size}> ${array_var};\n")
     file(WRITE ${source_file} "#include \"${array_var}Array.hpp\"\n\n${array_content}\n")
-    
+
     set(${out_hpp_file} ${header_file} PARENT_SCOPE)
     set(${out_cpp_file} ${source_file} PARENT_SCOPE)
 endfunction()
